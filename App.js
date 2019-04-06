065fb8677391fd5df63c5b8a35713d5e282ca328
christyyao <yao.christy@gmail.com>
2019-04-06 16:42:32 +0200

    app

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Splash from './src/components/Splash/Splash';
+import Login from './src/components/Login/Login';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text> Spotify Alarm wasssup </Text>
-      </View>
+      // <Splash/>
+      <Login/>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F1DFD7',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#251917',
+    fontSize: 35,
+    fontWeight: 'bold'
+  },
+  subtitle: {
+    color: '#A5866C',
+    fontWeight: '200',
+    paddingBottom: 20
+  },
+  titleWrapper: {
+    justifyContent: 'center',
+    flex: 1
+  }
 });
